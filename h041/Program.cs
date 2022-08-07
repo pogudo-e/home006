d495bded95f958570697d31232e04e53463fc402
Evgeny Pogudo <sensesbit@gmail.com>
2022-08-07 21:15:50 +0300

    first commit

---
+++ h041/Program.cs
@@ -0,0 +1,27 @@
+﻿// int age = Convert.ToInt32(Console.ReadLine());
+int m;
+Console.Write("Введите число элементов массива: ");
+m = Convert.ToInt16(Console.ReadLine());
+int [] array = new int[m];
+int i = 0;
+while (i < m)
+{
+    Console.Write("Введите элемент массива: ");
+    array[i] = int.Parse(Console.ReadLine()!);
+    Console.WriteLine();
+    i++;
+}
+
+void AboveZero (int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0){
+            result++;
+        }
+    }
+    Console.WriteLine($"Я насчитал: {result} положительных числа.");
+}
+
+AboveZero(array);
---
+++ h043/Program.cs
@@ -0,0 +1,11 @@
+﻿// y = k1 * x + b1, y = k2 * x + b2
+// 2 5 4 9
+
+double b1 = double.Parse(Console.ReadLine()!);
+double k1 = double.Parse(Console.ReadLine()!);
+double b2 = double.Parse(Console.ReadLine()!);
+double k2 = double.Parse(Console.ReadLine()!);
+
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"{x}, {y}");
